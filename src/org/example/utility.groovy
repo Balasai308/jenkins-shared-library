7cd2b5ec0a104309a5a637705c80543149eb3989
Balasai308 <pbalasai05@gmail.com>
2026-05-03 21:51:23 +0530

    Add Utils class for utility functions

--- src/org/example/utility.groovy
+++ src/org/example/utility.groovy
@@ -1,1 +1,13 @@
+package org.example
 
+class Utils implements Serializable {
+    def steps
+
+    Utils(steps) {
+        this.steps = steps
+    }
+
+    def printMessage(msg) {
+        steps.echo "Message: ${msg}"
+    }
+}
