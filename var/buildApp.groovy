507874d866b394e9667384c32131f68547f1b80a
Balasai308 <pbalasai05@gmail.com>
2026-05-03 22:22:17 +0530

    Update buildApp.groovy

--- var/buildApp.groovy
+++ var/buildApp.groovy
@@ -1,8 +1,3 @@
-def call(String appName) {
-    echo "Starting build for ${appName} 🚀"
-    
-    sh "echo Building ${appName}..."
-    sh "sleep 2"
-    
-    echo "Build completed for ${appName} ✅"
+def call(String name) {
+    echo "Hello ${name}"
 }
